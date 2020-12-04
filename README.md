9893ae50ea093c733579bea5fdd2e6b6e1598506
Pragy Agarwal <agar.pragy@gmail.com>
2020-12-04 21:37:21 +0530

    save all files

---
+++ README.md
@@ -0,0 +1,6 @@
+# Install ncurses
+
+linux: `sudo apt-get install libncurses6 libncurses6-dev`
+macos: `brew install ncurses`
+
+Reference: https://invisible-island.net/ncurses/man/ncurses.3x.html
--- ui.cpp
+++ ui.cpp
@@ -1,5 +1,11 @@
 #include "ui.h"
 
+// ncurses will save whatever you have on the screen
+// it will clear the screen
+// it will take control over the screen
+// when you exit, it will release control
+// and it will restore whatever you had earlier
+
 void init_ui() {
     initscr(); // initialize the ncurses mode
 }
@@ -9,7 +15,6 @@ void tear_down_ui() {
 }
 
 void game() {
-
     move(10, 10);
     addstr("Hello, I'm in ncurses"); // add a string to the window
     getch();
